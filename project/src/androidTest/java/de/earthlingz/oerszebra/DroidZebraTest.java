c7879e31625b821381a3dac34ff27c0b2d50a38a
oers <stmura+git@gmail.com>
2019-05-25 19:10:10 +0200

    Adapted testcases to new androidx.test

--- project/src/androidTest/java/de/earthlingz/oerszebra/DroidZebraTest.java
+++ project/src/androidTest/java/de/earthlingz/oerszebra/DroidZebraTest.java
@@ -1,68 +1,63 @@
 package de.earthlingz.oerszebra;
 
 import android.content.Intent;
-import android.test.ActivityInstrumentationTestCase2;
+import androidx.test.filters.SmallTest;
+import androidx.test.rule.ActivityTestRule;
 import com.shurik.droidzebra.ZebraEngine;
 import de.earthlingz.oerszebra.BoardView.GameStateBoardModel;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class de.earthlingz.oerszebra.DroidZebraTest \
- * de.earthlingz.oerszebra.tests/android.test.InstrumentationTestRunner
- */
-public class DroidZebraTest extends ActivityInstrumentationTestCase2<DroidZebra> {
+import static org.junit.Assert.assertSame;
 
-    public DroidZebraTest() {
-        super("de.earthlingz.oerszebra", DroidZebra.class);
-    }
+@SmallTest
+public class DroidZebraTest  {
 
-    
-    @Override
-    protected void setUp() throws Exception {
-    	super.setUp();
-        while (!getActivity().initialized()) {
+    private DroidZebra zebra;
+
+    @Rule
+    public ActivityTestRule<DroidZebra> activityRule
+            = new ActivityTestRule<>(DroidZebra.class);
+
+    @Before
+    public void init() throws InterruptedException {
+        zebra = activityRule.getActivity();
+        while (!zebra.initialized()) {
             Thread.sleep(100);
         }
     }
 
 
-    @Override
-    protected void tearDown() throws Exception {
-    	super.tearDown();
-    }
-
 //    public TestView_Functional_Test() {
 //    	super("project.base", TestView.class);
 //    }
 
+    @Test
     public void testSkipWithFinalEmptySquares() throws InterruptedException {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(Intent.EXTRA_TEXT, "F5D6C4D3C3F4F3E3E6C5F6G5H4E7C6F7G8F2G1E2F8H6F1D1D2D8D7C8C7B8G4C1C2H3G6H5G3H2G2B5A5H1B6H7B7A8A7A6G7E8H8A4B4E1B3A3B1");
 
-        this.getActivity().onNewIntent(intent);
+        zebra.onNewIntent(intent);
 
         Thread.sleep(500);
         assertSame(3, countSquares(ZebraEngine.PLAYER_EMPTY));
         assertSame(58, countSquares(ZebraEngine.PLAYER_WHITE));
         assertSame(3, countSquares(ZebraEngine.PLAYER_BLACK));
-        assertSame(this.getActivity().getState().getBlackScore(), 3);
-        assertSame(this.getActivity().getState().getWhiteScore(), 61);
+        assertSame(zebra.getState().getBlackScore(), 3);
+        assertSame(zebra.getState().getWhiteScore(), 61);
     }
 
+    @Test
     public void testSkipCompleteGame() throws InterruptedException {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(Intent.EXTRA_TEXT, "E6F4C3C4G3D6B4F5G6G5G4H5D7E3F2H4H3H2F3A4H6H7D3F6F7C5A3A2B5A5A6A7B6C6E7C7C8F1G7D8E8H8B8G8G2F8G1H1E1A8B7E2C2B1C1D1D2B3B2A1");
 
-        this.getActivity().onNewIntent(intent);
+        zebra.onNewIntent(intent);
         Thread.sleep(500);
         //this.getActivity().getEngine().waitForEngineState(ZebraEngine.ES_USER_INPUT_WAIT);
 
@@ -71,13 +66,14 @@ public class DroidZebraTest extends ActivityInstrumentationTestCase2<DroidZebra>
         assertSame(2, countSquares(ZebraEngine.PLAYER_BLACK));
     }
 
+    @Test
     public void testIssue22() throws InterruptedException {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(Intent.EXTRA_TEXT, "D3C5F6F5F4C3C4D2E2B4D1F3B5E3F2F1A4D6E6E7F7B6E8C6B3A5D7A3E1A6G1A2C2C7B8D8C8G8G6H6G5H5G4H4H3G7H8F8H7A8A7B7A1B2G3G2H2H1C1B1");
 
-        this.getActivity().onNewIntent(intent);
+        this.zebra.onNewIntent(intent);
         Thread.sleep(500);
         //this.getActivity().getEngine().waitForEngineState(ZebraEngine.ES_USER_INPUT_WAIT);
         assertSame(0, countSquares(ZebraEngine.PLAYER_EMPTY));
@@ -86,7 +82,7 @@ public class DroidZebraTest extends ActivityInstrumentationTestCase2<DroidZebra>
     }
 
     private int countSquares(byte color) {
-        GameStateBoardModel state = this.getActivity().getState();
+        GameStateBoardModel state = this.zebra.getState();
         int result = 0;
         for (int y = 0, boardLength = state.getBoardHeight(); y < boardLength; y++) {
             for (int x = 0, rowLength = state.getBoardRowWidth(); x < rowLength; x++) {
--- project/src/androidTest/java/de/earthlingz/oerszebra/InvalidmoveTest.java
+++ project/src/androidTest/java/de/earthlingz/oerszebra/InvalidmoveTest.java
@@ -3,62 +3,54 @@ package de.earthlingz.oerszebra;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.test.ActivityInstrumentationTestCase2;
+import androidx.test.rule.ActivityTestRule;
 import com.shurik.droidzebra.ZebraEngine;
 import de.earthlingz.oerszebra.BoardView.GameStateBoardModel;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 import java.lang.ref.WeakReference;
 
-/**
- * This is a simple framework for a test of an Application.  See
- * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
- * how to write and extend Application tests.
- * <p/>
- * To run this test, you can type:
- * adb shell am instrument -w \
- * -e class de.earthlingz.oerszebra.DroidZebraTest \
- * de.earthlingz.oerszebra.tests/android.test.InstrumentationTestRunner
- */
-public class InvalidmoveTest extends ActivityInstrumentationTestCase2<DroidZebra> {
-
-    public InvalidmoveTest() {
-        super("de.earthlingz.oerszebra", DroidZebra.class);
-    }
+import static org.junit.Assert.assertSame;
 
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        while (!getActivity().initialized()) {
+public class InvalidmoveTest {
+
+    private DroidZebra zebra;
+
+    @Rule
+    public ActivityTestRule<DroidZebra> activityRule
+            = new ActivityTestRule<>(DroidZebra.class);
+
+    @Before
+    public void init() throws InterruptedException {
+        zebra = activityRule.getActivity();
+        while (!zebra.initialized()) {
             Thread.sleep(100);
         }
     }
 
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
+    @Test
     public void testIssue24() throws InterruptedException {
         Intent intent = new Intent();
         intent.setAction(Intent.ACTION_SEND);
         intent.setType("message/rfc822");
         intent.putExtra(Intent.EXTRA_TEXT, "D3D4D5D6");
 
-        this.getActivity().onNewIntent(intent);
+        zebra.onNewIntent(intent);
         Thread.sleep(1000);
-        //this.getActivity().getEngine().waitForEngineState(ZebraEngine.ES_USER_INPUT_WAIT);
+        //zebra.getEngine().waitForEngineState(ZebraEngine.ES_USER_INPUT_WAIT);
 
         int countWait = 0;
-        while (getActivity().getAlert() == null && countWait < 100) {
+        while (zebra.getAlert() == null && countWait < 100) {
             Thread.sleep(100);
             countWait++;
         }
-        WeakReference<AlertDialog> alert = getActivity().getAlert();
+        WeakReference<AlertDialog> alert = zebra.getAlert();
         AlertDialog diag = alert.get();
 
-        getActivity().runOnUiThread(() -> diag.getButton(DialogInterface.BUTTON_POSITIVE).performClick());
+        zebra.runOnUiThread(() -> diag.getButton(DialogInterface.BUTTON_POSITIVE).performClick());
 
         Thread.sleep(1000);
         assertSame(60, countSquares(ZebraEngine.PLAYER_EMPTY));
@@ -68,7 +60,7 @@ public class InvalidmoveTest extends ActivityInstrumentationTestCase2<DroidZebra
     }
 
     private int countSquares(byte color) {
-        GameStateBoardModel state = this.getActivity().getState();
+        GameStateBoardModel state = zebra.getState();
         int result = 0;
         for (int y = 0, boardLength = state.getBoardHeight(); y < boardLength; y++) {
             for (int x = 0, rowLength = state.getBoardRowWidth(); x < rowLength; x++) {
